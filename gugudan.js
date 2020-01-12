5dd7de7ea789ab90bbe107ff29056bc21d4fc297
leejeongmee <alwjdal980413@naver.com>
2020-01-12 18:08:01 +0900

    구구단

--- gugudan.js
+++ gugudan.js
@@ -1,10 +1,16 @@
 function gugudan(start, end) {
-  for (var i = start; i <= end; i++) {
-    console.log(i + '단');
-    for (var j = 1; j <= 9; j++) {
-      console.log(i + ' * ' + j + ' = ' + i * j);
+  for (let i = start; i <= end; i++) {
+    //let const
+    //상수는 const
+    //변수는 var대신 let으로 하는 것이 더 좋음.
+    //둘의 차이는 let은 지역변수안에서만 살아있고 var은 전역변수에서 다 살아있는다.
+    console.log(`${i}단`);
+    //`${변수} 문자열`으로 하는 것이 더 좋음.
+    //``템플릿 리터럴(변수와 문자열을 같이 사용할 수 있음.)
+    for (let j = 1; j <= 9; j++) {
+      console.log(`${i} * ${j} = ${i * j}`);
     }
-    console.log(' ');
+    console.log(``);
   }
 }
 gugudan(3, 6);
