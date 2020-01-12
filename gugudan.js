ddde81028844a6b20e3bc4163629fefcca3b8208
shinhyunhwa <hhwa39@naver.com>
2020-01-12 17:57:54 +0900

    구구단

---
+++ gugudan.js
@@ -0,0 +1,11 @@
+
+function gugudan (start,end){
+    for (var a= start; a <end; a++){
+        console.log(a+'단');
+        for (var b=1; b<10; b++){
+            console.log(a +' * '+b +' = '+ a*b);
+        }
+        console.log(' ');
+    }
+}
+gugudan(2,6);
