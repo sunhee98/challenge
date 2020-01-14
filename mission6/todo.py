3d03989e9df5f03a5229bc91b5df1aefa7f91ec5
JeongHaeMin <44433577+JeongHM@users.noreply.github.com>
2020-01-14 23:25:53 +0900

    [refactor, feat] show command function 리팩토링 및 기능 구현

--- mission6/todo.py
+++ mission6/todo.py
@@ -1,38 +1,47 @@
-todo_list = [
-    {
-        'id': 123,
-        'values': ['lorem', 'xdof']
-    },
-    {
-        'id': 124,
-        'values': ['snffox', 'lorem']
-    }
-]
+todo_list = {
+    'todo': [
+        {
+            'id': 123,
+            'values': '자바스크립트 공부하기'
+        },
+        {
+            'id': 124,
+            'values': 'IOS 공부하기'
+        }
+    ],
+    'doing': [],
+    'done': []
+}
 
 
-def input_text():
-    text = input('명령하세요 : ')
-    command = text.split('&&')[0]
-    return command, text
+def make_unique_id():
+    import random
+    exist_ids = [v.get('id') for key, value in todo_list.items() for v in value]
+
+    for i in range(len(exist_ids) + 1):
+        r = random.randint(1, 1000)
+        if r not in exist_ids:
+            return r
+
+def split_text(text):
+    return {n: t for n, t in enumerate(text.split('$$'), start=1)}
 
 
+def show_command(commands):
+    second_command = commands.get(2)
+    if second_command == 'current':
+        return ''.join([f'{key}: {[v.get("id") for v in values]}, ' for key, values in todo_list.items()])
 
-def show_command(text):
-    if text == 'current':
-        pass
-    elif text == 'todo':
-        pass
-    else:
-        raise KeyError(f'{text} is not in command please check command')
+    elif second_command == 'todo':
+        count = len(todo_list.get(second_command))
+        values = ', '.join([f'"{item.get("values")}, {item.get("id")}"' for item in todo_list.get(second_command)])
+        return f'총{count}건 : {values}'
+
 
 def main():
-    command, text = input_text()
-    if command == 'show':
-        pass
-    elif command == 'add':
-        pass
-    elif command == 'delete':
-        pass
-    else:
-        raise KeyError(f'{text} is not in command please check command')
+    raw_command = input('명령하세요 : ')
+    commands = split_text(text=raw_command)
+    first_command = commands.get(1)
 
+    if first_command == 'show':
+        print(show_command(commands))
